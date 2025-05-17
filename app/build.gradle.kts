1d90588c0641a2020c8670ba7e8ee775fa325d27
Chantree <juan.carlos.chantre@correounivalle.edu.co>
2025-05-17 13:18:46 -0500

    feat:fix de bug

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,10 +74,7 @@ dependencies {
     implementation("androidx.activity:activity-ktx:1.7.2")
     implementation("androidx.fragment:fragment-ktx:1.5.7")
 
-    //livedata
-    implementation("androidx.lifecycle:lifecycly-livedata-ktx:2.6.1")
 
-    
 
 
 
