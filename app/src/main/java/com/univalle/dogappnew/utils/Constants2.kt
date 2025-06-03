d5c1a02d40cd4060feefbc5c875d5cba098b1964
Raquel Gonzalez <raquel.gonzalez@correounivalle.edu.com>
2025-06-02 22:28:00 -0500

    Feat: added navigation to Detalle_cita

---
+++ app/src/main/java/com/univalle/dogappnew/utils/Constants2.kt
@@ -0,0 +1,6 @@
+package com.univalle.dogappnew.utils
+
+object Constants2 {
+    const val BASE_URL1 = "https://dog.ceo/api/"
+    const val END_POINT1 = "breeds/image/random"
+}
--- app/src/main/java/com/univalle/dogappnew/view/fragments/FragmentHome.kt
+++ app/src/main/java/com/univalle/dogappnew/view/fragments/FragmentHome.kt
@@ -6,13 +6,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.univalle.dogappnew.R
 import com.univalle.dogappnew.databinding.FragmentDetalleCitaBinding
 import com.univalle.dogappnew.databinding.FragmentHomeBinding
+import com.univalle.dogappnew.model.Appointment
+import com.univalle.dogappnew.view.adapter.PetsAdapter
+import com.univalle.dogappnew.viewmodel.AppointmentViewModel
 
 class FragmentHome : Fragment() {
     private lateinit var binding: FragmentHomeBinding
+    private lateinit var viewModel: AppointmentViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,12 +31,28 @@ class FragmentHome : Fragment() {
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this).get(AppointmentViewModel::class.java)
+
         navigationFragmentNewAppointment()
+        observerListAppointments()
     }
 
     private fun navigationFragmentNewAppointment (){
         binding.fabAgregar.setOnClickListener{
             findNavController().navigate(R.id.action_fragmentHome2_to_fragmentNewAppointment)
+        }
+    }
+
+    private fun observerListAppointments(){
+        viewModel.getAllAppointments()
+        viewModel.listAppointments.observe(viewLifecycleOwner){
+            list ->
+            var recycler = binding.rvPets
+            val layoutManager = LinearLayoutManager(context)
+            recycler.layoutManager = layoutManager
+            val adapter = PetsAdapter(list, findNavController())
+            recycler.adapter = adapter
+            //adapter.notifyDataSetChanged()
         }
     }
 }
